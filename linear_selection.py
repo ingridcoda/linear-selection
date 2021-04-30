b719ed972b9354fd2f4ef533e0c23519cee0f3c6
Ingrid Coda <ingrid.coda@zoop.com.br>
2021-04-29 22:40:37 -0300

    General adjusts and creating tests

--- linear_selection.py
+++ linear_selection.py
@@ -1,29 +1,35 @@
-import math
-
-
 def linear_selection(a, k):
     if len(a) == 1:
         return a[0]
     M = get_medians_list(a)
-    print("M", M, "ceil", math.ceil(len(M) / 2))
-    median = linear_selection(M, math.ceil(len(M) / 2))
-    print("median", median)
+    # print("M", M, "ceil", len(M) // 2)
+    median = linear_selection(M, len(M) // 2)
+    # print("median", median)
     L = []
     R = []
     for item in a:
+        # print("ITEM", item, "MEDIAN", median)
         if item < median:
             L.append(item)
+            # print("if 1", L, R)
+        elif item == median and len(L) <= len(R):
+            L.append(item)
+            # print("if 2", L, R)
+        elif item == median and len(R) < len(L):
+            R.append(item)
+            # print("if 3", L, R)
         else:
             R.append(item)
-    print(L, R)
+            # print("if 4", L, R)
+    # print(L, R)
     if len(L) == k - 1:
-        print("Mediana final", median)
+        # print("Mediana final", median, "k", k, L, R)
         return median
     if len(L) > k - 1:
-        print("segundo if", len(L), len(R), L, R, k, median)
+        # print("segundo if", len(L), len(R), L, R, k, median)
         return linear_selection(L, k)
     if len(L) < k - 1:
-        print("terceiro if", len(L), len(R), L, R, k, median)
+        # print("terceiro if", len(L), len(R), L, R, k, median)
         return linear_selection(R, k - len(L))
 
 
@@ -41,26 +47,20 @@ def get_medians_list(a):
 
     for i in range(0, len(lists)):
         lists[i].sort()
-        print(lists[i])
+        # print("LISTA", i, lists[i])
         medians_list.append(lists[i][len(lists[i]) // 2])
-        print(lists[i][len(lists[i]) // 2])
+        # print("MEDIANA LISTA", i, lists[i][len(lists[i]) // 2])
 
-    print(medians_list)
+    # print(medians_list)
     return medians_list
 
 
-# vet = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-# print(median_of_medians(vet, len(vet) // 2))
+# vet = [8, 7, 6, 5, 4, 3, 2, 2, 2, 1]
+# # k = math.ceil(len(vet) / 2)
+# k = 5
+# print(f"Getting {k}-th item from {vet} using Linear Selection...")
+# result = linear_selection(vet, k)
+# print(f"Original array: {vet}")
 # vet.sort()
-# print(vet)
-
-# vet = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-# print(temp_median(vet))
-# vet.sort()
-# print(vet)
-
-
-vet = [1, 2, 2, 3, 4, 5]
-print(linear_selection(vet, math.ceil(len(vet) / 2)))
-vet.sort()
-print(vet)
+# print(f"Sorted array: {vet}")
+# print(f"Result: {result}")
