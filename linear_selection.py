2b44667cfe125032582fc71c1478fd17b08e13f8
Ingrid Coda <ingrid.coda@zoop.com.br>
2021-05-02 19:22:37 -0300

    General adjusting and calculating time averages

--- linear_selection.py
+++ linear_selection.py
@@ -2,34 +2,24 @@ def linear_selection(a, k):
     if len(a) == 1:
         return a[0]
     M = get_medians_list(a)
-    # print("M", M, "ceil", len(M) // 2)
     median = linear_selection(M, len(M) // 2)
-    # print("median", median)
     L = []
     R = []
     for item in a:
-        # print("ITEM", item, "MEDIAN", median)
         if item < median:
             L.append(item)
-            # print("if 1", L, R)
         elif item == median and len(L) <= len(R):
             L.append(item)
-            # print("if 2", L, R)
         elif item == median and len(R) < len(L):
             R.append(item)
-            # print("if 3", L, R)
         else:
             R.append(item)
-            # print("if 4", L, R)
-    # print(L, R)
+
     if len(L) == k - 1:
-        # print("Mediana final", median, "k", k, L, R)
         return median
     if len(L) > k - 1:
-        # print("segundo if", len(L), len(R), L, R, k, median)
         return linear_selection(L, k)
     if len(L) < k - 1:
-        # print("terceiro if", len(L), len(R), L, R, k, median)
         return linear_selection(R, k - len(L))
 
 
@@ -47,20 +37,6 @@ def get_medians_list(a):
 
     for i in range(0, len(lists)):
         lists[i].sort()
-        # print("LISTA", i, lists[i])
         medians_list.append(lists[i][len(lists[i]) // 2])
-        # print("MEDIANA LISTA", i, lists[i][len(lists[i]) // 2])
 
-    # print(medians_list)
     return medians_list
-
-
-# vet = [8, 7, 6, 5, 4, 3, 2, 2, 2, 1]
-# # k = math.ceil(len(vet) / 2)
-# k = 5
-# print(f"Getting {k}-th item from {vet} using Linear Selection...")
-# result = linear_selection(vet, k)
-# print(f"Original array: {vet}")
-# vet.sort()
-# print(f"Sorted array: {vet}")
-# print(f"Result: {result}")
--- tests.py
+++ tests.py
@@ -4,24 +4,29 @@ import time
 from linear_selection import linear_selection
 from sort_selection import sort_selection
 
-
 n = 1000
 step = 1000
-max = 10000
-
+_max = 10000
 vets = []
-for i in range(10):
-    while n <= max:
+
+print("Populando vetores...")
+while n <= _max:
+    for i in range(10):
         vet = []
         while len(vet) < n:
             num = random.randint(1, 100000)
             if num not in vet:
                 vet.append(num)
         vets.append(vet)
-        n += step
+    n += step
+print("Vetores populados.")
 
 l_times = []
 s_times = []
+count = 0
+countByLen = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+countLinearErrorsByLen = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+countSortErrorsByLen = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 for vet in vets:
     start_time = time.time()
     linear = linear_selection(vet, len(vet) // 2)
@@ -29,7 +34,31 @@ for vet in vets:
     start_time = time.time()
     sort = sort_selection(vet, len(vet) // 2)
     s_times.append((time.time() - start_time))
+
     if linear != sort:
-        # raise Exception("ERRÔ!", len(vet), vet)
-        print("ERRÔ!", len(vet), vet)
-    print("linear == sort ?", linear, sort, linear == sort)
+        vet.sort()
+        print("ERRÔ!", len(vet), "linear == sort ?", linear, sort, linear == sort,
+              vet[len(vet) // 2], linear == vet[len(vet) // 2], sort == vet[len(vet) // 2])
+        count += 1
+        countByLen[(len(vet) // 1000) - 1] += 1
+        if linear != vet[len(vet) // 2]:
+            countLinearErrorsByLen[(len(vet) // 1000) - 1] += 1
+        if sort != vet[len(vet) // 2]:
+            countSortErrorsByLen[(len(vet) // 1000) - 1] += 1
+    else:
+        print(len(vet), "linear == sort ?", linear, sort, linear == sort)
+
+averagesLinearByLen = []
+averagesSortByLen = []
+j = 0
+for i in range(10):
+    averagesLinearByLen.append(sum(l_times[j:j + 10]) / 10)
+    averagesSortByLen.append(sum(s_times[j:j + 10]) / 10)
+    j += 10
+
+print("Total diferentes:", count)
+print("Total diferentes por tam:", countByLen)
+print("Total diferentes linear por tam:", countLinearErrorsByLen)
+print("Total diferentes sort por tam:", countSortErrorsByLen)
+print("Medias linear por tam:", averagesLinearByLen)
+print("Medias sort por tam:", averagesSortByLen)
